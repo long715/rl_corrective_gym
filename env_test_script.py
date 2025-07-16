af1c3497dd0b84ba87dda66cbe74f63e7b1282e5
long715 <long715@aucklanduni.ac.nz>
2025-07-16 16:49:22 +1200

    feat: updated the test script

--- env_test_script.py
+++ env_test_script.py
@@ -7,18 +7,28 @@ This also contains test training scripts for the gym.
 """
 
 import gymnasium as gym
+import numpy as np
 
 from corrective_transfer_env import CorrectiveTransferEnvironment
 
 
 def test_prop():
-    # Tests if the propagator works without perturbation and control
+    """
+    Tests if the propagator works without perturbation and control
+    - guided and unguided should be the same in this case so expected reward is ZERO
+    - the propagated state should be close to the desired final state since no perturbations
+    """
     env: gym.Env = CorrectiveTransferEnvironment("SCP_impulsive_traj.csv", "SCP_dV.csv")
     final_state, reward, _, _, _ = env.step([-1, 1, 1, 1])
-    print(env.nominal_traj[-1, :] - final_state, reward)
+    state_diff: np.ndarray = env.nominal_traj[-1, :] - final_state
 
     # error ~1e-7 which could be attributed to rounding within propagator
-    assert abs(reward) < 1e-6, "Propagator error exceeds acceptable threshold"
+    assert (
+        np.linalg.norm(state_diff) < 1e-6
+    ), "Propagator error exceeds acceptable threshold"
+    assert reward == 0, "Error between guided and unguided trajectory"
+
+    print("PASS")
 
 
 if __name__ == "__main__":
