fix: error computation in reward function

corrective_transfer_env.py
@@ -53,6 +53,7 @@ class CorrectiveTransferEnvironment(gym.Env):
 
         # reward config
         self.penalty_scale_control: float = 10.0
+        self.penalty_scale_dynamics: float = 10.0
 
         # define the spaces ie. all possible range of obs and action
         # NOTE: 2*au should be sufficient for the application of mars transfer
@@ -204,6 +205,9 @@ class CorrectiveTransferEnvironment(gym.Env):
         guid_xf: np.ndarray,
         no_guid_xf: np.ndarray,
     ) -> float:
+        """
+        TODO: normalise and scale the rewards
+        """
         reward: float = 0
 
         # penalty for exceeding the control limits
@@ -212,7 +216,7 @@ class CorrectiveTransferEnvironment(gym.Env):
             reward += control_diff * self.penalty_scale_control
 
         # reward/penalty for dynamics
-        nom_rv_final: np.ndarray = self.nominal_traj
+        nom_rv_final: np.ndarray = self.nominal_traj[-1, :]
         error_no_guid: np.ndarray = no_guid_xf - nom_rv_final
         error_guid: np.ndarray = guid_xf - nom_rv_final
 